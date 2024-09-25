3e5ef2ac5789780b4a58f66e3a2de8e3b3740634
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-09-26 00:46:59 +0300

    Define function registerUser to create a new user

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,47 @@
 const User = require('../models/User');
 const asyncHandler = require('express-async-handler');
 const generateToken = require('../utils/generateToken');
+
+// @desc    Register a new user
+// @route   POST /api/users/register
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { firstName, lastName, phoneNumber, email, username, password, role } = req.body;
+
+    // Check if the user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  // Create a new user
+  const user = await User.create({
+    firstName,
+    lastName,
+    phoneNumber,
+    email,
+    username,
+    password,
+    role,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      phoneNumber: user.phoneNumber,
+      email: user.email,
+      username: user.username,
+      role: user.role,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
+module.exports = { registerUser };
