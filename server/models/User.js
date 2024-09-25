4e9def3b962685e7ed4672a8c2e8de16ab8ad629
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-09-25 23:48:44 +0300

    Define the user schema

--- server/models/User.js
+++ server/models/User.js
@@ -2,4 +2,38 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 // Define the user schema
-const userSchema = new mongoose.Schema();
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['administrator', 'teacher', 'student', 'parent'],
+    required: true,
+  },
+}, {
+  timestamps: true
+});
