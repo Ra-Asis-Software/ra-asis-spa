878579a5fd74ef14346db04b04fa227f0ae6d219
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-09-27 00:13:51 +0300

    Modify form structure and layout

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -36,99 +37,109 @@ const Register = () => {
   };
 
   return (
-    <div>
-      <h2>Register</h2>
-      <form onSubmit={onSubmit}>
-        <div>
-          <label>
-            <input
-              type="radio"
-              name="role"
-              value="student"
-              checked={role === 'student'}
-              onChange={onChange}
-            />
-            Student
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="role"
-              value="teacher"
-              checked={role === 'teacher'}
-              onChange={onChange}
-            />
-            Teacher
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="role"
-              value="parent"
-              checked={role === 'parent'}
-              onChange={onChange}
-            />
-            Parent
-          </label>
+    <div className='registration'>
+        <div className='registration-intro'>
+            <h2>Register</h2>
         </div>
-        <input
-          type="text"
-          name="firstName"
-          value={firstName}
-          placeholder="First Name"
-          onChange={onChange}
-          required
-        />
-        <input
-          type="text"
-          name="lastName"
-          value={lastName}
-          placeholder="Last Name"
-          onChange={onChange}
-          required
-        />
-        <input
-          type="text"
-          name="phoneNumber"
-          value={phoneNumber}
-          placeholder="Phone Number"
-          onChange={onChange}
-          required
-        />
-        <input
-          type="email"
-          name="email"
-          value={email}
-          placeholder="Email"
-          onChange={onChange}
-          required
-        />
-        <input
-          type="text"
-          name="username"
-          value={username}
-          placeholder="Username"
-          onChange={onChange}
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          value={password}
-          placeholder="Password"
-          onChange={onChange}
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          value={password}
-          placeholder="Confirm Password"
-          onChange={onChange}
-          required
-        />
-        <button type="submit">Submit Details</button>
-      </form>
+        <div className='registration-form'>
+            <form onSubmit={onSubmit}>
+                <div className='user-category'>
+                    <div className='user-category-heading'>
+                        <h3>Please select a user category</h3>
+                    </div>
+                    <div className='user-category-choices'>
+                        <label>
+                            <input
+                            type="radio"
+                            name="role"
+                            value="student"
+                            checked={role === 'student'}
+                            onChange={onChange}
+                            />
+                            Student
+                        </label>
+                        <label>
+                            <input
+                            type="radio"
+                            name="role"
+                            value="teacher"
+                            checked={role === 'teacher'}
+                            onChange={onChange}
+                            />
+                            Teacher
+                        </label>
+                        <label>
+                            <input
+                            type="radio"
+                            name="role"
+                            value="parent"
+                            checked={role === 'parent'}
+                            onChange={onChange}
+                            />
+                            Parent
+                        </label>
+                    </div>
+                </div>
+                <div className='personal-details'>
+                    <input
+                    type="text"
+                    name="firstName"
+                    value={firstName}
+                    placeholder="First Name"
+                    onChange={onChange}
+                    required
+                    />
+                    <input
+                    type="text"
+                    name="lastName"
+                    value={lastName}
+                    placeholder="Last Name"
+                    onChange={onChange}
+                    required
+                    />
+                    <input
+                    type="text"
+                    name="phoneNumber"
+                    value={phoneNumber}
+                    placeholder="Phone Number"
+                    onChange={onChange}
+                    required
+                    />
+                </div>
+                <div className='credential-details'>
+                    <input
+                    type="email"
+                    name="email"
+                    value={email}
+                    placeholder="Email"
+                    onChange={onChange}
+                    required
+                    />
+                    <input
+                    type="text"
+                    name="username"
+                    value={username}
+                    placeholder="Username"
+                    onChange={onChange}
+                    required
+                    />
+                    <input
+                    type="password"
+                    name="password"
+                    value={password}
+                    placeholder="Password"
+                    onChange={onChange}
+                    required
+                    />
+                </div>
+                <div className='submit-button'>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+        <div className='login-prompt'>
+            <p>Already have an account ? <Link to='/login'>Login</Link></p>
+        </div>
     </div>
   );
 };
