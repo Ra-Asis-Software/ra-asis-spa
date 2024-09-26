0684a4169d950c93bc9b1c53dd7039ada1abdd32
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-09-26 15:33:14 +0300

    Setup routing for navigation
    - Made necessary imports from react and react-router dom
    - Import and use Register component
    - Create a component for the Homepage/Landing Page and link to the Registration page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Register from './components/Register';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to The Ra'Asis Student Progress Analytics App</h1>
-    </div>
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+const Home = () => {
+
+  return(
+    <>
+      <div className='home-introduction'>
+        <h1>Why Hustle ? Forget About Tiresome Manual Academic Progress Analysis. Ra'Asis SPA Does It For You!</h1>
+        <Link to='/register'>Sign Up Now</Link>
+      </div>
+    </>
   );
 }
 
