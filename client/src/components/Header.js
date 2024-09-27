12c7ad1f90ee5e9106271616c0d6d8a87694d438
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-09-27 12:38:33 +0300

    Change logo and icon image

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <header>
             <div className='app-logo'>
                 <Link to="/">
-                    <img src="/assets/raasis_logo.svg" alt="SPA logo" />
+                    <img src="/assets/spa_logo.svg" alt="SPA logo" />
                 </Link>
             </div>
         </header>
