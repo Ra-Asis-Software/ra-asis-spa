f27f3a311e69197dec027890ff9a8beafda00105
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-09-29 19:51:35 +0300

    Define routes for email verification

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { registerUser, validateRegister } = require('../controllers/authController');
+const { registerUser, validateRegister, verifyEmail } = require('../controllers/authController');
 
 const router = express.Router();
 
 // Route for user registration
 router.post('/register', validateRegister, registerUser);
+
+//Route for email verification
+router.get('/verify-email/:token', verifyEmail);
 
 
 module.exports = router;
