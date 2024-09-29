71a034f7f4f4773f728d5d3aaa0f1edbdfce8b38
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-09-30 02:05:07 +0300

    Initial Commit - user dashboard component

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dashboard = () => {
+
+    return(
+        <>
+            <h2>Dashboard</h2>
+            <h3>Hello User</h3>
+        </>
+    );
+};
+
+export default Dashboard;
