a982e41a64647014945d4484e42b42135bc2c364
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-09-29 19:44:26 +0300

    Initial Commit - nodemailer app to send verification emails

---
+++ server/utils/sendMail.js
@@ -0,0 +1,22 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USERNAME, // Sender email address (We will have to setup email ya SPA for this)
+      pass: process.env.EMAIL_PASSWORD, // Email App password (using my Gmail App Password for testing)
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USERNAME,
+    to: options.email, // Recipient email
+    subject: options.subject, // Email subject
+    html: options.message, // Email content in HTML format
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
