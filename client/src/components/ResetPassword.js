ca132218e4f041205658423fdc8a854913820bd7
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-10-10 19:13:52 +0300

    Restructure password reset request elements

--- client/src/components/ResetPassword.js
+++ client/src/components/ResetPassword.js
@@ -32,21 +32,36 @@ const ResetPassword = () => {
           <p>We have sent password recovery instructions to your email.</p>
         </div>
       ) : (
-        <form onSubmit={handleSubmit}>
-          <h1>Forgot Password?</h1>
-          <p>Enter the email address associated with your account, and we’ll send you a link to reset your password.</p>
-          <input
-            type="email"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <button type="submit" disabled={loading}>
-            {loading ? 'Sending...' : 'Get Link'}
-          </button>
-          {status === 'error' && <small>Something went wrong when sending reset link. Please try again.</small>}
-        </form>
+        <>
+            <div className="reset-password-intro">
+                <p>
+                    Enter the email address associated with your account, and we will send you a link to reset your password
+                </p>
+            </div>
+            <div className="request-reset-form">
+                <form onSubmit={handleSubmit} noValidate>
+                    <div className="email-input">
+                        <input
+                            type="email"
+                            placeholder="Enter your email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="get-link-btn">
+                        <button type="submit" disabled={loading}>
+                            {loading ? 'Preparing Link...' : 'Get Link'}
+                        </button>
+                    </div>
+                    <div className="form-message">
+                        {status === 'error' && <small className="error-message">Something went wrong when sending reset link. Please try again.</small>}
+                    </div>
+                </form>
+            </div>
+            <div className="reset-password-img">
+                <img src="/assets/password_reset.webp" alt="" />
+            </div>
+        </>
       )}
     </div>
   );
