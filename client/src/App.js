cb4bce8ca9fbe04a20c82bc81a6ddb24b8e5e0f6
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-10-10 17:47:23 +0300

    Import and use UpdatePassword component

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Register from './components/Register';
 import VerifyEmail from './components/VerifyEmail';
 import Login from './components/Login';
 import ResetPassword from './components/ResetPassword';
+import UpdatePassword from './components/UpdatePassword';
 import Dashboard from './components/Dashboard';
 
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
           <Route path='/verify-email/:token' element={ <VerifyEmail /> } />
           <Route path='/login' element={ <Login /> } />
           <Route path='/reset-password' element={ <ResetPassword /> } />
+          <Route path="/reset-password/:token" element={<UpdatePassword />} />
           <Route path='/dashboard' element={ <Dashboard /> } />
         </Routes>
       </div>
