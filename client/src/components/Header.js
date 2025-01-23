91f733e009a6362d0831cfe2e03d4efa54994f9c
Allan Kimutai Tum <allankimutaitum@gmail.com>
2025-01-23 18:28:10 +0300

    Add navigation bar and styling for landing page header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,12 +4,20 @@ import { Link } from "react-router-dom";
 const Header = () => {
 
     return(
-        <header>
+        <header className="landing-header" id="landing_header">
             <div className="app-logo">
                 <Link to="/">
                     <img src="/assets/spa_logo.svg" alt="SPA logo" />
                 </Link>
             </div>
+            <nav className="main-navigation">
+                <Link to="#">WHAT WE OFFER<i className="fa-solid fa-chevron-down"></i></Link>
+                <Link to="#">SUPPORT</Link>
+                <Link to="#">COMPANY</Link>
+                <Link to="#">RESOURCES</Link>
+                <Link to="#">SEARCH</Link>
+                <Link to="#">REGISTER / LOGIN</Link>
+            </nav>
         </header>
     );
 }
