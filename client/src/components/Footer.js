e37825fa05d97f67d6481c7e13ec2f1ab6b3c9e8
Allan Kimutai Tum <allankimutaitum@gmail.com>
2025-01-24 01:07:03 +0300

    Initial commit - component for landing page footer content

---
+++ client/src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="landing-footer" id="landing">
+            <div className="">
+
+            </div>
+            <div className="">
+                
+            </div>
+            <div className="">
+                
+            </div>
+            <div className="">
+                
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
