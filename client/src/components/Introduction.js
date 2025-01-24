ba7defc9e2bc191d86ea0e2eaf736b73259e897b
Allan Kimutai Tum <allankimutaitum@gmail.com>
2025-01-24 15:56:29 +0300

    Initial commit - component for landing page introduction content

---
+++ client/src/components/Introduction.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Introduction = () => {
+    return (
+        <div className="landing-intro">
+            <h1>Empower Student Success With Real-Time Analytics</h1>
+        </div>
+    );
+}
+
+export default Introduction;
