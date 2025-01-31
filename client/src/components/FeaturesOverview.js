6d2cd9399096f43a77be00422520bce8fdb511e4
Allan Kimutai Tum <allankimutaitum@gmail.com>
2025-01-31 09:49:14 +0300

    Initial commit - landing page component for app's features ovevriew

---
+++ client/src/components/FeaturesOverview.js
@@ -0,0 +1,13 @@
+import React from "react-router-dom";
+
+const FeaturesOverview = () => {
+    return (
+        <div className="features-overview">
+            <div className="features-heading">
+                <h2>Features Overview</h2>
+            </div>
+        </div>
+    );
+}
+
+export default FeaturesOverview;
