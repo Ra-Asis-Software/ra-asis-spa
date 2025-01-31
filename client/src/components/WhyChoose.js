ad8e9e414bdc413887ba94b292d7d4e806781f9b
Allan Kimutai Tum <allankimutaitum@gmail.com>
2025-01-31 13:14:48 +0300

    Initial commit - component for 'why choose the app content'

---
+++ client/src/components/WhyChoose.js
@@ -0,0 +1,30 @@
+import React from "react-router-dom";
+
+const WhyChoose = () => {
+    return (
+        <div className="why-choose">
+            <div className="why-choose-heading">
+                <h2>Why Choose Ra'Asis Analytica</h2>
+            </div>
+            <div className="why-choose-reasons">
+                <div className="reason-box">
+
+                </div>
+                <div className="reason-box">
+
+                </div>
+                <div className="reason-box">
+
+                </div>
+                <div className="reason-box">
+
+                </div>
+                <div className="reason-box">
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default WhyChoose;
