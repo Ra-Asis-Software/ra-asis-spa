16480df17945d33bae620bab23bbe836c961851e
Allan Kimutai Tum <allankimutaitum@gmail.com>
2025-03-12 16:06:09 +0300

    Implement Header component responsive design

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+    // State to track opening and closing of responsive menu
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const location = useLocation(); // Will use this to track current route to set active class
+
+    // Toggle opening and closing menu
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    // Close menu on user selection
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    }
 
     return(
         <header className="landing-header" id="landing_header">
@@ -10,14 +23,62 @@ const Header = () => {
                     <img src="/assets/spa_logo.svg" alt="SPA logo" />
                 </Link>
             </div>
-            <nav className="main-navigation">
-                <Link to="#">WHAT WE OFFER<i className="fa-solid fa-chevron-down"></i></Link>
-                <Link to="#">SUPPORT</Link>
-                <Link to="#">COMPANY</Link>
-                <Link to="#">RESOURCES</Link>
-                <Link to="#">SEARCH</Link>
-                <Link to="/register">REGISTER</Link>
-                <Link to="/login">LOGIN</Link>
+
+            {/* Mobile Menu Icon */}
+            <div className="mobile-menu-icon" onClick={toggleMenu}>
+                <i className={`fa-solid ${isMenuOpen ? "fa-xmark" : "fa-bars"}`}></i>
+            </div>
+
+            <nav className={`main-navigation ${isMenuOpen ? "active" : ""}`}>
+                <Link 
+                    to="#" 
+                    className={location.pathname === "#" ? "active" : ""} 
+                    onClick={closeMenu}
+                >
+                    WHAT WE OFFER<i className="fa-solid fa-chevron-down"></i>
+                </Link>
+                <Link 
+                    to="#" 
+                    className={location.pathname === "#" ? "active" : ""} 
+                    onClick={closeMenu}
+                >
+                    SUPPORT
+                </Link>
+                <Link 
+                    to="#" 
+                    className={location.pathname === "#" ? "active" : ""} 
+                    onClick={closeMenu}
+                >
+                    COMPANY
+                </Link>
+                <Link 
+                    to="#" 
+                    className={location.pathname === "#" ? "active" : ""} 
+                    onClick={closeMenu}
+                >
+                    RESOURCES
+                </Link>
+                <Link 
+                    to="#" 
+                    className={location.pathname === "#" ? "active" : ""} 
+                    onClick={closeMenu}
+                >
+                    SEARCH
+                </Link>
+                <Link 
+                    to="/register" 
+                    className={location.pathname === "/register" ? "active" : ""} 
+                    onClick={closeMenu}
+                >
+                    REGISTER
+                </Link>
+                <Link 
+                    to="/login" 
+                    className={location.pathname === "/login" ? "active" : ""} 
+                    onClick={closeMenu}
+                >
+                    LOGIN
+                </Link>
             </nav>
         </header>
     );
