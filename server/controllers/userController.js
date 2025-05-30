3835c4916da66339dbc602bfb52bcf223388a454
Jimmy Rubia <rubiajimmy3@gmail.com>
2025-05-30 12:32:44 +0300

    finalize getStudent controller. submissions, assignments, and events sections in the controller to be handled later

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,8 @@
 import AsyncHandler from 'express-async-handler'
 import Student from '../models/Student.js'
 import User from '../models/User.js'
+import '../models/Assignment.js'
+import '../models/Submission.js'
 
 //@desc get student details
 // @route   GET /api/users/student/:id
@@ -9,6 +11,12 @@ export const getStudent = AsyncHandler( async(req, res) => {
     const { id } = req.params
 
     const student = await Student.findOne({ bio: id })
+    .populate('bio')
+    .populate({
+        path: 'units',
+        populate: { path: 'assignments' }
+    })
+    .populate('submissions')
 
     if(!student) {
         //get profile only if the specific user model has not been instantiated
@@ -29,6 +37,15 @@ export const getStudent = AsyncHandler( async(req, res) => {
     //return user details if the specific user model is found
     return res.status(200).json({
         message: "User details successfully retrieved",
-        data: student
+        data: {
+            profile: student.bio,
+            units: student.units.map(unit => {
+                return { id: unit._id, name: unit.unitName, code: unit.unitCode }
+            }),
+            assignments: student.units.flatMap(unit => unit.assignments),
+            submissions: student.submissions,
+            events: student.calender,
+
+        }
     })
 })
