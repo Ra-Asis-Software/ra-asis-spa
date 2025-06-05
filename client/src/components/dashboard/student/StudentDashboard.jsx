1f6c4ed49b39028ca653098e652a67640dd97f7b
Jimmy Rubia <rubiajimmy3@gmail.com>
2025-06-05 14:58:02 +0300

    add header and sidebar to TeacherDashboard

--- client/src/components/dashboard/student/StudentDashboard.jsx
+++ client/src/components/dashboard/student/StudentDashboard.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import Sidebar from '../SideBar';
-import Header from '../Header';
-import StudentMain from './StudentMain';
-import styles from '../css/Dashboard.module.css';
+import React, { useState } from "react";
+import Sidebar from "../SideBar";
+import Header from "../Header";
+import StudentMain from "./StudentMain";
+import styles from "../css/Dashboard.module.css";
 
-const StudentDashboard = ({profile}) => {
+const StudentDashboard = ({ profile }) => {
   const [showNav, setShowNav] = useState(false);
-  const [selectedSubject, setSelectedSubject] = useState('Mathematics');
+  const [selectedSubject, setSelectedSubject] = useState("Mathematics");
 
   return (
     <div className={styles.dashboardContainer}>
       <Header
-        {...{profile}}
+        {...{ profile }}
         setShowNav={setShowNav}
         showNav={showNav}
         selectedSubject={selectedSubject}
@@ -19,7 +19,11 @@ const StudentDashboard = ({profile}) => {
       />
       <div className={styles.contentWrapper}>
         <Sidebar show={showNav} />
-        <StudentMain showNav={showNav} subject={selectedSubject} profile={profile} />
+        <StudentMain
+          showNav={showNav}
+          subject={selectedSubject}
+          profile={profile}
+        />
       </div>
     </div>
   );
