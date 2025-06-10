213ad07207600b6b877e4d83a9997beee17b91dc
Jimmy Rubia <rubiajimmy3@gmail.com>
2025-06-10 14:56:51 +0300

    add src/services for making requests, start teacher dashboard

--- client/src/components/dashboard/teacher/TeacherDashboard.jsx
+++ client/src/components/dashboard/teacher/TeacherDashboard.jsx
@@ -1,34 +1,34 @@
-import React, { useState } from 'react'
-import Header from '../Header'
-import styles from '../css/Dashboard.module.css'
-import Sidebar from '../SideBar';
-import TeacherMain from './TeacherMain';
-import ViewAssignment from '../ViewAssignment';
+import React, { useState } from "react";
+import Header from "../Header";
+import styles from "../css/Dashboard.module.css";
+import Sidebar from "../SideBar";
+import TeacherMain from "./TeacherMain";
+import ViewAssignment from "../ViewAssignment";
 
-function TeacherDashboard({profile}) {
-    const [showNav, setShowNav] = useState(false);
-    const [selectedSubject, setSelectedSubject] = useState('Mathematics');
-    const[showAssignments, setShowAssignments] = useState(false)
-    return (
-        <div className={styles.dashboardContainer}>
-           <Header
-            {...{profile}}
-            setShowNav={setShowNav}
-            showNav={showNav}
-            selectedSubject={selectedSubject}
-            setSelectedSubject={setSelectedSubject} />
+function TeacherDashboard({ profile }) {
+  const [showNav, setShowNav] = useState(false);
+  const [selectedSubject, setSelectedSubject] = useState("Mathematics");
+  const [showAssignments, setShowAssignments] = useState(false);
+  return (
+    <div className={styles.dashboardContainer}>
+      <Header
+        {...{ profile }}
+        setShowNav={setShowNav}
+        showNav={showNav}
+        selectedSubject={selectedSubject}
+        setSelectedSubject={setSelectedSubject}
+      />
 
-            <div className={styles.contentWrapper}>
-                <Sidebar show={showNav} />
-                {
-                    !showAssignments ?
-                    <TeacherMain {...{showNav, profile, setShowAssignments}} /> :
-                    <ViewAssignment />
-                }
-                
-            </div>
-        </div>
-    )
+      <div className={styles.contentWrapper}>
+        <Sidebar show={showNav} />
+        {!showAssignments ? (
+          <TeacherMain {...{ showNav, profile, setShowAssignments }} />
+        ) : (
+          <ViewAssignment />
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default TeacherDashboard
+export default TeacherDashboard;
