b90c6ec4d8f7d08fa7949103597cec0315791e97
Jimmy Rubia <rubiajimmy3@gmail.com>
2025-06-27 09:25:24 +0300

    implement all, assigned, enrolled, search units -for all users

--- client/src/components/dashboard/Units.jsx
+++ client/src/components/dashboard/Units.jsx
@@ -7,12 +7,15 @@ import RoleRestricted from "../ui/RoleRestricted";
 const Units = ({ user }) => {
   const [allUnits, setAllUnits] = useState([]);
   const [units, setUnits] = useState([]);
+  const [selectedUnits, setSelectedUnits] = useState([]);
+  const [unitsHolder, setUnitsHolder] = useState([]);
   useEffect(() => {
     const fetchUnits = async () => {
       const unitsFetched = await getAllUnits();
 
       if (unitsFetched.data.success) {
         setAllUnits(unitsFetched.data.data);
+        setUnitsHolder(unitsFetched.data.data);
 
         const myUnits = await getUserDetails(user.role, user.id);
         if (myUnits.data.data.units) {
@@ -22,13 +25,51 @@ const Units = ({ user }) => {
     };
     fetchUnits();
   }, []);
+
+  const handleSelectUnit = (e, unit) => {
+    //if unit already is selected, dont add
+    if (e.target.checked) {
+      if (!selectedUnits.includes(unit.unitCode)) {
+        setSelectedUnits((prev) => [...prev, unit.unitCode]);
+      }
+    }
+
+    //if target is unchecked, check if it exists first
+    if (!e.target.checked) {
+      if (selectedUnits.includes(unit.unitCode)) {
+        const tempArray = selectedUnits.filter(
+          (item) => item !== unit.unitCode
+        );
+        setSelectedUnits(tempArray);
+      }
+    }
+  };
+
+  const handleSearchUnit = (value) => {
+    if (value === "") {
+      setAllUnits(unitsHolder);
+    } else {
+      const searchResults = allUnits.filter(
+        (unit) =>
+          unit.unitCode.toLowerCase().includes(value.toLowerCase()) ||
+          unit.unitName.toLowerCase().includes(value.toLowerCase())
+      );
+
+      setAllUnits(searchResults);
+    }
+  };
+
   return (
     <div className={styles.hero}>
       <div className={styles.heroLeft}>
         <h3>Units</h3>
         <div className={styles.searchBox}>
           <i className={`fa-solid fa-search ${styles.faSearch}`}></i>
-          <input type="text" placeholder="search unit" />
+          <input
+            type="text"
+            placeholder="search unit"
+            onChange={(e) => handleSearchUnit(e.target.value)}
+          />
           <i className={`fa-solid fa-x ${styles.faCancel}`}></i>
         </div>
         <div className={styles.unitsBox}>
@@ -41,7 +82,10 @@ const Units = ({ user }) => {
               return (
                 <div key={unit._id} className={styles.unit}>
                   <div className={styles.unitDetails}>
-                    <input type="checkbox" />
+                    <input
+                      type="checkbox"
+                      onChange={(e) => handleSelectUnit(e, unit)}
+                    />
                     <p>{unit.unitName}</p>
                   </div>
                   <p>{unit.unitCode}</p>
@@ -63,15 +107,21 @@ const Units = ({ user }) => {
           <div className={styles.assigned}>
             {units.map((unit) => {
               return (
-                <div key={unit.id} className={styles.assignedUnit}>
+                <button key={unit.id} className={styles.assignedUnit}>
                   {unit.name}
-                </div>
+                </button>
               );
             })}
           </div>
           <h4>Selected</h4>
           <div className={styles.assigned}>
-            <p>No units selected</p>
+            {selectedUnits.length === 0 ? (
+              <p>No units selected</p>
+            ) : (
+              selectedUnits.map((unit) => {
+                return <div className={styles.assignedUnit}>{unit}</div>;
+              })
+            )}
           </div>
         </div>
       </div>
