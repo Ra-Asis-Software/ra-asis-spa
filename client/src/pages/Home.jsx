c1d0d59a233dc52b81979492c07ef09f6dbf5369
owinogabriel <g73346920@gmail.com>
2025-10-08 13:52:07 +0300

    feat: add authentication check to Home component.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Header from "../components/layout/Header";
 import Introduction from "../components/home/Introduction";
 import FeaturesOverview from "../components/home/FeaturesOverview";
@@ -6,15 +7,23 @@ import Testimonials from "../components/home/Testimonials";
 import LeadGeneration from "../components/home/LeadGeneration";
 import ContactForm from "../components/home/ContactForm";
 import Footer from "../components/layout/Footer";
+import { useEffect } from "react";
 
 const Home = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // State to track if user === logged in
+  // Check localStorage for authentication token when component mounts
+  useEffect(() => {
+    const token = localStorage.getItem("authToken");
+    setIsLoggedIn(!!token); // Set isLoggedIn to true if token exists, false otherwise
+  }, []);
+
   return (
     <>
       <Header />
       <Introduction />
       <FeaturesOverview />
       <WhyChoose />
-      <Testimonials />
+      <Testimonials isAuthenticated={isLoggedIn} />
       <LeadGeneration />
       <ContactForm />
       <Footer />
